0020d63e1c189bde8abe3a2a7429fa91ab20e220
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-04-29 20:23:28 -0600

    Remove or disable everything #extern

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 # Run these tests on every target that could be used for a NAT front-end
-# (also to test (#extern) on multiple targets)
 haxe test.hxml py.hxml &&
 haxe test.hxml js.hxml &&
 haxe test.hxml cpp.hxml &&
