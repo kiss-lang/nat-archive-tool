5fa52bf8e0f588fdb63a1ac219e43e7c9ae78faf
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-04-29 21:13:20 -0600

    install hxnodejs when testing

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+if [ -n "$CI_OS_NAME" ]; then
+    lix install haxelib:hxnodejs
+fi
+
 # Run these tests on every target that could be used for a NAT front-end
 haxe test.hxml py.hxml &&
 haxe test.hxml js.hxml &&
