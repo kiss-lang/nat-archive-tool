fcfa9b9de24a36020b78a92132f12741522d3e33
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-12-04 12:44:38 -0700

    resurrect WikipediaImageSystem with lime

--- src/nat/systems/MediaWikiSystem.hx
+++ src/nat/systems/MediaWikiSystem.hx
@@ -6,6 +6,11 @@ import nat.System;
 import haxe.Json;
 import haxe.ds.Option;
 
+#if lime
+import lime.net.HTTPRequest;
+#end
+
+
 /**
  * Base System that processes Entries using the HTTP API for any wiki built with MediaWiki
  */
